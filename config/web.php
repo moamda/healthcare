9674a7553203478bf8caa0489ea6d2cd77161369
Moham <mohamnotmad@gmail.com>
2024-10-08 16:09:30 +0800

    init

--- config/web.php
+++ config/web.php
@@ -19,8 +19,7 @@ $config = [
             'class' => 'mdm\admin\components\AccessControl',
             'allowActions' => [
                 'site/*',
-                'admin/*',
-                'some-controller/some-action',
+                // 'admin/*',
                 // The actions listed here will be allowed to everyone including guests.
                 // So, 'admin/*' should not appear here in the production, of course.
                 // But in the earlier stages of your development, you may probably want to
